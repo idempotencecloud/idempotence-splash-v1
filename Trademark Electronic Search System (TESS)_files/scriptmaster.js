6ae221bd28c184b142d88530e0449ccbd9ff39bb
Asad <asadhasan@Asads-MBP.hsd1.tx.comcast.net>
2023-02-18 10:15:29 -0600

    Upload snapshot for page.

---
+++ Trademark Electronic Search System (TESS)_files/scriptmaster.js
@@ -0,0 +1,35 @@
+if( window.location.hostname.toLowerCase() == "www.uspto.gov" ) 
+{
+	document.write("<script src='https:\/\/www.uspto.gov\/includes\/j\/uspto_head_1.js'><\/script>");
+}
+document.write("<table width=\"100%\" border=\"0\" cellpadding=\"3\" cellspacing=\"0\" bgcolor=\"#001D2D\">");
+document.write("<tr valign=\"middle\"><td width=\"59\" rowspan=\"2\" align=\"left\" valign=\"middle\">");
+document.write("<a href=\"#skipnav\"><img src=\"https:\/\/www.uspto.gov\/images\/blank.gif\" alt=\"skip navigation\" ");
+document.write("width=\"1\" height=\"10\" border=\"0\" align=\"absmiddle\"><\/a>");
+document.write("<a href=\"https:\/\/www.uspto.gov\/index.html\">");
+document.write("<img src=\"https:\/\/www.uspto.gov\/images\/uspto_seal_50.gif\" alt=\"U S P T O Seal [home]\" width=\"50\" height=\"50\" border=\"0\" align=\"middle\"><\/a>");
+document.write("<\/td><td align=\"left\" valign=\"bottom\" class=\"xdarkbg\">United States Patent and Trademark Office<\/td><\/tr>");
+document.write("<tr bgcolor=\"#002032\" valign=\"middle\">");
+document.write("<td align=\"left\" valign=\"bottom\" bgcolor=\"#001D2D\">");
+document.write("<a href=\"https://www.uspto.gov\" class=\"darkbg\">Home<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/sitemap\" class=\"darkbg\">Site Index<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://search.uspto.gov/search?utf8=%E2%9C%93&affiliate=web-sdmg-uspto.gov&sort_by=&query=uspto+search\" class=\"darkbg\">Search<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/learning-and-resources/general-faqs\" class=\"darkbg\" title=\"Frequently Asked Questions\">FAQ<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/learning-and-resources/glossary\" class=\"darkbg\">Glossary<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/about-us/contact-us\" class=\"darkbg\">Contacts<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/learning-and-resources/support-centers/patent-electronic-business-center\" class=\"darkbg\"><em><strong>e<\/strong><\/em>Business<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/blog/ebiz/\" class=\"darkbg\">eBiz alerts<\/a>");
+document.write("<span class=\"vertbar\">|<\/span>");
+document.write("<a href=\"https://www.uspto.gov/about-us/news-updates\" class=\"darkbg\">News<\/a>");
+document.write("<a name=\"skipnav\" id=\"skipnav\">");
+document.write("<img src=\"https:\/\/www.uspto.gov\/images\/blank.gif\" alt=\"\" width=\"15\" height=\"1\" border=\"0\" ><\/td><\/tr><\/table> ");
+
+
+
